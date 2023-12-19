1177c144d9baf93c6ad475e4c550fc5d835d92a9
Muhammed Fettah Başdemir <basdemirfettah@gmail.com>
2023-12-19 19:19:17 +0300

    sort test

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -23,12 +23,12 @@ const CardList = async ({ page, cat }) => {
 
 	const hasPrev = POST_PER_PAGE * (page - 1) > 0;
 	const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
-
+	const sortedPosts = posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 	return (
 		<div className={styles.container}>
 			<h1 className={styles.title}>Recent Posts</h1>
 			<div className={styles.posts}>
-				{posts?.map((item) => (
+				{sortedPosts?.map((item) => (
 					<Card item={item} key={item._id} />)
 				)}
 			</div>
