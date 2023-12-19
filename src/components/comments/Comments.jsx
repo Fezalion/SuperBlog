b91e401caf7320b08e183aab4c60afdd97beb497
Muhammed Fettah Başdemir <basdemirfettah@gmail.com>
2023-12-19 16:51:09 +0300

    changed localhost to fettahb.me

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -22,7 +22,7 @@ const fetcher = async (url) => {
 
 const Comments = ({ postSlug }) => {
   const { status } = useSession()
-  const { data, mutate, isLoading } = useSWR(`http://localhost:3000/api/comments?postSlug=${postSlug}`, fetcher);
+  const { data, mutate, isLoading } = useSWR(`fettahb.me/api/comments?postSlug=${postSlug}`, fetcher);
   const [desc, setDesc] = useState("");
   const handleSubmit = async () => {
     await fetch("/api/comments", {
