ebfbc837fa814577c96d1684d0d30cba24e7fa0c
Muhammed Fettah Başdemir <basdemirfettah@gmail.com>
2023-12-28 10:55:23 +0300

    about me test

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -11,7 +11,17 @@ const AboutPage = () =>{
   </div>
   <div className={styles.textContainer}>
     <h2>Muhammed Fettah Başdemir</h2>
-    <p>Sucukları severim ve bazen iyi kod yazarım. Ama genelde yazdığım kodlara spagetti demek bile övgü sayılır.</p>
+    <p>Merhaba, ben [Adınız Soyadınız]!</p>
+    <p></p>
+    <p>🚀 Yazılım Geliştirici ve [Sizin Uzmanlık Alanınız]</p>
+    <p>🌐 Web Teknolojileri ve Uygulama Geliştirme ile ilgileniyorum</p>
+    <p>💼 Şu anda [Şirket Adı] firmasında çalışıyorum</p>
+    <p>🔧 Ana uzmanlık alanlarım: [Ana Uzmanlık Alanlarınız]</p>
+    <p>🚀 Sürekli öğrenmeye ve gelişen teknolojilere ayak uydurmaya odaklıyım</p>
+    <p>📚 Kendimi geliştirmek için düzenli olarak [Okuma, Kurslar, Seminerler] gibi kaynaklardan yararlanıyorum</p>
+    <p>📢 Ayrıca, [Sosyal Medya Hesaplarınız veya Kişisel Blogunuz] üzerinden yazılım ve teknolojiyle ilgili düşüncelerimi paylaşıyorum<br/></p>
+    <p>🌍 İletişim kurmak ve yeni projelerde işbirliği yapmak beni heyecanlandırıyor, bu yüzden benimle iletişime geçmekten çekinmeyin!<br/></p>
+    <p>edefim, teknoloji dünyasındaki değişimlere ayak uydurarak, sürekli öğrenerek ve deneyimlerimi paylaşarak hem kişisel hem de profesyonel olarak gelişmek. Eğer benimle aynı tutkulara sahipseniz, birlikte çalışmayı çok isterim!</p>
   </div>
 </div>
 
