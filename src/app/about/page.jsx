611aa871f6514e26bdb2eedd5a1f4a9229244879
Muhammed Fettah Başdemir <basdemirfettah@gmail.com>
2023-12-28 10:18:51 +0300

    about test

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./aboutPage.module.css"
+
+
+const AboutPage = () =>{
+    return(
+        <div>
+            <h1>TESTINGTESTING</h1>
+        </div>
+    )
+}
+
+export default AboutPage
