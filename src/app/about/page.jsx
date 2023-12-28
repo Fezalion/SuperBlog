c75c5f283d8ed4661420c9329a52a0049a47230b
Muhammed Fettah Başdemir <basdemirfettah@gmail.com>
2023-12-28 10:28:54 +0300

    about me image test

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,6 +6,9 @@ const AboutPage = () =>{
     return(
         <div>
             <h1>TESTINGTESTING</h1>
+            <div className={styles.imgContainer}>
+            <Image src={"/fettah.png"} alt="" classname={styles.image}/>
+            </div>
         </div>
     )
 }
